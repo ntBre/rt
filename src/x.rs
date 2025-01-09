0c64a373cfab3c78d391bc3fe72de9bd46e9361b
Brent Westbrook <brentrwestbrook@gmail.com>
2025-01-09 12:51:04 -0500

    impl ximopen and callbacks

--- src/x.rs
+++ src/x.rs
@@ -1,5 +1,5 @@
 use std::{
-    ffi::{c_char, c_double, c_int, c_long},
+    ffi::{c_char, c_double, c_int, c_long, c_void},
     mem::MaybeUninit,
     ptr::null_mut,
 };
@@ -7,15 +7,17 @@ use std::{
 use x11::xlib::{
     InputHint, NorthEastGravity, NorthWestGravity, PBaseSize, PMaxSize,
     PMinSize, PResizeInc, PSize, PWinGravity, SouthEastGravity,
-    SouthWestGravity, Success, USPosition, XNegative, XUTF8StringStyle, XValue,
-    YNegative, YValue,
+    SouthWestGravity, Success, USPosition, XIMPreeditNothing, XIMStatusNothing,
+    XNegative, XUTF8StringStyle, XValue, YNegative, YValue,
 };
 
 use crate::{
     between,
     bindgen::{
         self, borderpx, opt_class, opt_name, opt_title, termname, win, xw,
-        XAllocSizeHints, XClassHint, XWMHints,
+        XAllocSizeHints, XClassHint, XCreateIC, XICCallback, XIMCallback,
+        XNDestroyCallback, XPointer, XSetIMValues, XVaCreateNestedList,
+        XWMHints, XIC, XIM,
     },
 };
 
@@ -110,9 +112,87 @@ pub(crate) fn xloadcols() {
     unsafe { bindgen::xloadcols() }
 }
 
-// DUMMY
-pub(crate) fn ximopen(dpy: *mut bindgen::Display) -> c_int {
-    unsafe { bindgen::ximopen(dpy) }
+pub(crate) fn ximopen(_dpy: *mut bindgen::Display) -> c_int {
+    unsafe {
+        let mut imdestroy =
+            XIMCallback { client_data: null_mut(), callback: Some(ximdestroy) };
+        let icdestroy =
+            XICCallback { client_data: null_mut(), callback: Some(xicdestroy) };
+
+        xw.ime.xim =
+            bindgen::XOpenIM(xw.dpy, null_mut(), null_mut(), null_mut());
+        if xw.ime.xim.is_null() {
+            return 0;
+        }
+
+        if !XSetIMValues(
+            xw.ime.xim,
+            XNDestroyCallback,
+            &raw mut imdestroy,
+            null_mut::<c_void>(),
+        )
+        .is_null()
+        {
+            eprintln!("XSetIMValues: Could not set XNDestroyCallback.");
+        }
+
+        // NOTE these variadic functions take key-value pairs, terminated by
+        // NULL. the types of the keys must be *exactly* right or this will
+        // segfault. using the keys from x11 causes a segfault, so these must be
+        // the bindgen versions even if they superficially look compatible. the
+        // bindgen versions seem to be [u8], while the x11 versions are &str.
+        xw.ime.spotlist = XVaCreateNestedList(
+            0,
+            bindgen::XNSpotLocation,
+            &raw mut xw.ime.spot,
+            null_mut::<c_void>(),
+        );
+
+        if xw.ime.xic.is_null() {
+            xw.ime.xic = XCreateIC(
+                xw.ime.xim,
+                bindgen::XNInputStyle,
+                XIMPreeditNothing | XIMStatusNothing,
+                bindgen::XNClientWindow,
+                xw.win,
+                bindgen::XNDestroyCallback,
+                &icdestroy,
+                null_mut::<c_void>(),
+            );
+        }
+
+        if xw.ime.xic.is_null() {
+            eprintln!("XCreateIC: Could not create input context.");
+        }
+
+        1
+    }
+}
+
+extern "C" fn ximdestroy(_xim: XIM, _client: XPointer, _call: XPointer) {
+    unsafe {
+        xw.ime.xim = null_mut();
+        bindgen::XRegisterIMInstantiateCallback(
+            xw.dpy,
+            null_mut(),
+            null_mut(),
+            null_mut(),
+            Some(ximinstantiate),
+            null_mut(),
+        );
+        bindgen::XFree(xw.ime.spotlist);
+    }
+}
+
+extern "C" fn xicdestroy(
+    _xim: XIC,
+    _client: XPointer,
+    _call: XPointer,
+) -> c_int {
+    unsafe {
+        xw.ime.xic = null_mut();
+        1
+    }
 }
 
 // DUMMY
